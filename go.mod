c100b9a01bcafb157e56ebbb16e7df6d1e3cbcbf
tgnike <43302514+tgnike@users.noreply.github.com>
2023-01-30 20:59:38 +0300

    +заглушки для обработчиков

--- go.mod
+++ go.mod
@@ -8,6 +8,14 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+require (
 	github.com/caarlos0/env/v6 v6.10.1
 	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
--- internal/authservice/authhandlers/authhandlers.go
+++ internal/authservice/authhandlers/authhandlers.go
@@ -1,9 +1,12 @@
 package authhandlers
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/tgnike/yandex-praktikum-diploma/internal/models"
 	"github.com/tgnike/yandex-praktikum-diploma/internal/server"
 )
 
@@ -13,8 +16,34 @@ type RegistationHandler struct {
 }
 
 func (rh *RegistationHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	body, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var userJSON models.UserJSON
+
+	if err := json.Unmarshal(body, &userJSON); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	response := models.RegistrationResponse{Result: true}
+
+	resBody, err := json.Marshal(&response)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("it works! RegistationHandler"))
+	w.Write(resBody)
 }
 
 type LoginHandler struct {
@@ -23,6 +52,31 @@ type LoginHandler struct {
 }
 
 func (lh *LoginHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var userJSON models.UserJSON
+
+	if err := json.Unmarshal(body, &userJSON); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	response := models.LoginResponse{Token: "12345678"}
+
+	resBody, err := json.Marshal(&response)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("it works! LoginHandler"))
+	w.Write(resBody)
 }
